2628bc2604efac0fab0bc69d20873f4826a664fd
Satori31022809947 <3102809947@qq.com>
2025-06-27 21:24:52 +0800

    GameMgr

---
+++ Assets/Scripts/GameMgr.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMgr : MonoBehaviour
+{
+    private static GameMgr instance;
+    public static GameMgr Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameMgr>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("GameMgr");
+                    instance = obj.AddComponent<GameMgr>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public Button startGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (startGameButton != null)
+        {
+            startGameButton.onClick.AddListener(StartGame);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void StartGame()
+    {
+    }
+
+    public void EndGame()
+    {
+    }
+}
