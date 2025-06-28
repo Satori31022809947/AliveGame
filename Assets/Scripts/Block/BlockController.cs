d117357d684cdd9e2c8e3e4fbca441b26dceaca5
zdyb <xinkebiao912@163.com>
2025-06-28 12:09:23 +0800

    refactor: block data move to BlockController

---
+++ Assets/Scripts/Block/BlockController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    public BlockData blockData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
