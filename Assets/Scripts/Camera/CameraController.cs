ea45425f55e7b4d87e1fa903d24b178e708f4525
Satori31022809947 <3102809947@qq.com>
2025-06-28 14:42:21 +0800

    add camera control

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Vector3 offset; // 相机相对于玩家的偏移量
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (playerTransform == null)
+        {
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerTransform != null)
+        {
+            Vector3 newPosition = playerTransform.position + offset;
+            transform.position = newPosition;
+        }
     }
 }
