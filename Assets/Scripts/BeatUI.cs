e73c4d3ad16e86083c5e71d8fb61260eb8fcf83f
Satori31022809947 <3102809947@qq.com>
2025-06-29 10:07:20 +0800

    update beatUI

--- Assets/Scripts/BeatUI.cs
+++ Assets/Scripts/BeatUI.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,8 +9,12 @@ public class BeatUI : MonoBehaviour
 
     [SerializeField] private GameObject DangerEffect;
     [SerializeField] private GameObject WarningEffect;
+    [SerializeField] private GameObject EndPos;
     [SerializeField] private Text beatRatioText; // 新增：用于显示比值的 Text 组件
     [SerializeField] private Slider beatProgressSlider; // 新增：用于显示条状进度的 Slider 组件
+    [SerializeField] private Vector2 startPosition; // 修改为 RectTransform 类型
+    [SerializeField] private Vector2 endPosition; // 修改为 RectTransform 类型
+    [SerializeField] private float moveDuration = 1f; // 新增：移动时长
 
     private void Start()
     {
@@ -42,13 +48,25 @@ public class BeatUI : MonoBehaviour
             float ratio = (float)beatIndex / GameMgr.Instance.BeatLimit;
             beatProgressSlider.value = ratio;
         }
-        HideAllObjects();
+
+        if (EndPos != null)
+        {
+            EndPos.SetActive(true);
+        }
         ShowObjectBasedOnIndex(beatIndex);
         ShowEffect(beatType);
     }
 
     private void ShowEffect(BeatType beatType)
     {
+        if (WarningEffect != null)
+        {
+            WarningEffect.SetActive(false);
+        }
+        if (DangerEffect != null)
+        {
+            DangerEffect.SetActive(false);
+        }
         
         switch (beatType)
         {
@@ -85,16 +103,34 @@ public class BeatUI : MonoBehaviour
         {
             DangerEffect.SetActive(false);
         }
+        EndPos.SetActive(false);
     }
 
     private void ShowObjectBasedOnIndex(int beatIndex)
     {
-        HideAllObjects();
         int indexToShow = beatIndex % 3;
 
         if (indexToShow >= 0 && indexToShow < objectsToShow.Length && objectsToShow[indexToShow] != null)
         {
-            objectsToShow[indexToShow].SetActive(true);
+            var targetRect = objectsToShow[indexToShow].GetComponent<RectTransform>();
+            if (targetRect != null && startPosition != null && endPosition != null)
+            {
+                objectsToShow[indexToShow].SetActive(true);
+                targetRect.anchoredPosition = startPosition;
+                StartCoroutine(MoveObject(targetRect, startPosition, endPosition, moveDuration));
+            }
         }
+    }
+
+    private IEnumerator MoveObject(RectTransform target, Vector2 startPos, Vector2 endPos, float duration)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < duration)
+        {
+            target.anchoredPosition = Vector2.Lerp(startPos, endPos, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        target.anchoredPosition = endPos;
     }
 }
